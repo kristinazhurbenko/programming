51ccaed919ef8920385c5c8f03fcb4fef035b682
Kristina <kpe@ciklum.com>
2014-12-04 11:28:35 +0200

    DisplayInfo deleted

--- src/main/java/MatrixGUI.java
+++ src/main/java/MatrixGUI.java
@@ -1,11 +1,7 @@
 
-import javax.swing.JTextField;
+import javax.swing.*;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *

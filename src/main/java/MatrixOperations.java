79a69efb0d8d7917bd8270afe3f95cc2515a2a16
Kristina <kpe@ciklum.com>
2014-12-06 11:44:42 +0200

    multithread

--- src/main/java/MatrixOperations.java
+++ src/main/java/MatrixOperations.java
@@ -1,81 +1,20 @@
 /**
  * Created by 1 on 02.12.2014.
  */
-import java.util.Scanner;
+import javax.swing.*;
 
 public class MatrixOperations {
 
-    Scanner scan;
-    int matrix1[][], matrix2[][], result[][];
     int row, column;
 
-    void create() {
+    int[][] add(int[][] matrix1, int[][] matrix2) {
+        row = matrix1.length;
+        column = matrix1[0].length;
 
-        scan = new Scanner(System.in);
+        int[][] result = new int[row][column];
+        for (int i = 0; i < row; i++) {
 
-        System.out.println("Matrix Addition");
-
-        // First Matrix Creation..
-        System.out.println("\nEnter number of rows & columns");
-        row = Integer.parseInt(scan.nextLine());
-        column = Integer.parseInt(scan.nextLine());
-
-        matrix1 = new int[row][column];
-        matrix2 = new int[row][column];
-        result = new int[row][column];
-
-        System.out.println("Enter the data for first matrix :");
-
-        for(int i=0; i<row; i++) {
-
-            for(int j=0; j<column; j++) {
-
-                matrix1[i][j] = scan.nextInt();
-            }
-        }
-
-        // Second Matrix Creation..
-        System.out.println("Enter the data for second matrix :");
-
-        for(int i=0; i<row; i++) {
-
-            for(int j=0; j<column; j++) {
-
-                matrix2[i][j] = scan.nextInt();
-            }
-        }
-    }
-
-    void display() {
-
-        System.out.println("\nThe First Matrix is :");
-
-        for(int i=0; i<row; i++) {
-
-            for(int j=0; j<column; j++) {
-
-                System.out.print("\t" + matrix1[i][j]);
-            }
-            System.out.println();
-        }
-
-        System.out.println("\n\nThe Second Matrix is :");
-
-        for(int i=0; i<row; i++) {
-
-            for(int j=0; j<column; j++) {
-
-                System.out.print("\t" + matrix2[i][j]);
-            }
-            System.out.println();
-        }
-    }
-
-    void add() {
-
-        for(int i=0; i<row; i++) {
-
-            for(int j=0; j<column; j++) {
+            for (int j = 0; j < column; j++) {
 
                 result[i][j] = matrix1[i][j] + matrix2[i][j];
             }
@@ -83,21 +22,25 @@ public class MatrixOperations {
 
         System.out.println("\n\nThe Sum is :");
 
-        for(int i=0; i<row; i++) {
+        for (int i = 0; i < row; i++) {
 
-            for(int j=0; j<column; j++) {
+            for (int j = 0; j < column; j++) {
 
                 System.out.print("\t" + result[i][j]);
             }
             System.out.println();
         }
+        return result;
     }
 
-    void sub() {
+    int[][] sub(int[][] matrix1, int[][] matrix2) {
+        row = matrix1.length;
+        column = matrix1[0].length;
 
-        for(int i=0; i<row; i++) {
+        int[][] result = new int[row][column];
+        for (int i = 0; i < row; i++) {
 
-            for(int j=0; j<column; j++) {
+            for (int j = 0; j < column; j++) {
 
                 result[i][j] = matrix1[i][j] - matrix2[i][j];
             }
@@ -105,21 +48,25 @@ public class MatrixOperations {
 
         System.out.println("\n\nThe Subtraction is :");
 
-        for(int i=0; i<row; i++) {
+        for (int i = 0; i < row; i++) {
 
-            for(int j=0; j<column; j++) {
+            for (int j = 0; j < column; j++) {
 
                 System.out.print("\t" + result[i][j]);
             }
             System.out.println();
         }
+        return result;
     }
 
-    void multi() {
+    int[][] multi(int[][] matrix1, int[][] matrix2) {
+        row = matrix1.length;
+        column = matrix1[0].length;
 
-        for(int i=0; i<row; i++) {
+        int[][] result = new int[row][column];
+        for (int i = 0; i < row; i++) {
 
-            for(int j=0; j<column; j++) {
+            for (int j = 0; j < column; j++) {
 
                 result[i][j] = matrix1[i][j] * matrix2[i][j];
             }
@@ -127,35 +74,45 @@ public class MatrixOperations {
 
         System.out.println("\n\nThe Multiplication is :");
 
-        for(int i=0; i<row; i++) {
+        for (int i = 0; i < row; i++) {
 
-            for(int j=0; j<column; j++) {
+            for (int j = 0; j < column; j++) {
 
                 System.out.print("\t" + result[i][j]);
             }
             System.out.println();
         }
+        return result;
     }
 
-    void div() {
+    int[][] div(int[][] matrix1, int[][] matrix2) {
+        row = matrix1.length;
+        column = matrix1[0].length;
 
-        for(int i=0; i<row; i++) {
+        int[][] result = new int[row][column];
+        for (int i = 0; i < row; i++) {
 
-            for(int j=0; j<column; j++) {
+            for (int j = 0; j < column; j++) {
+                try {
+                    result[i][j] = matrix1[i][j] / matrix2[i][j];
+                } catch (ArithmeticException e) {
+                    System.out.println("\n\nThe Division to zero is not allowed!");
+                    JOptionPane.showMessageDialog(null, "The Division to zero is not allowed!");
+                }
 
-                result[i][j] = matrix1[i][j] / matrix2[i][j];
             }
         }
 
         System.out.println("\n\nThe Division is :");
 
-        for(int i=0; i<row; i++) {
+        for (int i = 0; i < row; i++) {
 
-            for(int j=0; j<column; j++) {
+            for (int j = 0; j < column; j++) {
 
                 System.out.print("\t" + result[i][j]);
             }
             System.out.println();
         }
+        return result;
     }
 }
